382957eec352587e1906ca0c8841cd719eba6c8b
20141105072 <2196690899@qq.com>
2016-06-20 10:43:42 +0800

    print
    用swift进行简单的输出，同时打印hello world，用var定义变量a，b和sum，不需要在每一句后面加分号，最后输出a＋b＝2

---
+++ helloworld/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  helloworld
+//
+//  Created by 20141105072 on 16/6/20.
+//  Copyright © 2016年 20141105072. All rights reserved.
+//
+
+import Foundation
+var a=1
+var b=1
+var sum=a+b
+print("Hello, World!")
+print(a+b)
+
